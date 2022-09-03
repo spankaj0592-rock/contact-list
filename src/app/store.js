bed2162823583e578a1c1be3620f3f5fe96d4c1b
spankajchauhan <96768418+spankajchauhan@users.noreply.github.com>
2022-09-03 12:21:38 +0530

    a

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import userSlice from '../features/contact/userSlice';
+import counterReducer from '../features/counter/counterSlice';
+
 export const store = configureStore({
   reducer: {
-  //  user:userSlice
+    counter: counterReducer,
   },
 });
